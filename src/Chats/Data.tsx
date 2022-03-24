00e48c99322eae50f74806d68a069020102bd296
l.e.nigmatullina <myueltwo@gmail.com>
2022-03-24 19:46:46 +0500

    add data

---
+++ src/Chats/Data.tsx
@@ -0,0 +1,19 @@
+export interface IUser {
+    id: number;
+    name: string;
+}
+
+export interface IChats {
+
+}
+
+export const contacts: IUser[] = [{
+    id: 1,
+    name: 'Alice'
+}, {
+    id: 2,
+    name: 'Tom'
+}, {
+    id: 3,
+    name: 'Jack'
+}];
