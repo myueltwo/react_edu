75c38a12702fbe1c0dd0eed959c6a60848bca70f
l.e.nigmatullina <myueltwo@gmail.com>
2022-03-29 01:14:45 +0500

    add data

--- src/Chats/Chats.tsx
+++ src/Chats/Chats.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import './Chats.scss';
+import {chats, IChats} from './Data';
+
+interface IChatsState {
+    chatsList: IChats[];
+    selectedChatId: string;
+}
+
 export default class Chats extends React.Component {
+    state: IChatsState;
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            chatsList: [...chats],
+            selectedChatId: chats[0].id
+        }
+    }
+
     render() {
+        const chatsList = [];
+        for (let i = 0; i < this.state.chatsList.length ; i++) {
+            chatsList.push(
+                <div className="board-row"
+                     key={'board-row' + i}
+                >
+                    {this.state.chatsList[i].name}
+                </div>
+            );
+        }
         return (
             <div className="react_edu-chats-main">
-                <div className="react_edu-chats-main__list"></div>
+                <div className="react_edu-chats-main__list">
+                    {chatsList}
+                </div>
                 <div className="react_edu-chats-main__correspondence"></div>
             </div>
         );
